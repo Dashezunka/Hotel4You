8f3808706566410460bacf40c6cd047a5174cb62
Ilya Bykov <il.byckoff2011@hotmail.com>
2018-01-22 22:17:30 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import logging
+
+from telegram import Location
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Filters, MessageHandler, RegexHandler
+
+import config
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+def start(bot, update, chat_data):
+    chat_data.clear()
+    update.message.reply_text('Здравствуйте')
+
+if __name__ == '__main__':
+    updater = Updater(config.TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start, pass_chat_data=True))
+
+    updater.start_polling()
+    updater.idle()
